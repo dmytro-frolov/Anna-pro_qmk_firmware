82aaeda8d3a17b255aa0be0d1870d70b6aeba4b1
dmytro.frolov <dmytro.frolov@outlook.com>
2026-04-26 18:16:27 +0300

    Sync current state

---
+++ keyboards/annepro2/keymaps/nvxd8c/config.h
@@ -0,0 +1,14 @@
+// Obins stock firmware has something similar to this already enabled, but disabled by default in QMK
+#define PERMISSIVE_HOLD
+
+//#pragma once
+//
+//#define UCIS_MAX_CODE_POINTS 12
+//
+//#define TAPPING_TOGGLE 3
+//
+//#define TAPPING_TERM 270
+//
+//// Some programs will not register TAP keys properly if they are triggered
+//// too quickly.
+//#define TAP_CODE_DELAY 70
