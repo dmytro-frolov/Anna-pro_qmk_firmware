afcdea7ee98aeda739a9e5669d7b6bf1723d13c9
Dmytro Frolov <dmytro.frolov@outlook.com>
2026-08-30 15:42:43 +0300

    Keep currently active lighting profile across USB sleep without resetting to 0

--- keyboards/annepro2/keymaps/purple_haze/keymap.c
+++ keyboards/annepro2/keymaps/purple_haze/keymap.c
@@ -45,39 +45,21 @@ enum anne_pro_layers {
  * │ Sleep│ BT 1 │ BT 2 │ BT 3 │ BT 4 │      │      │      │LEDOFF│LED ON│LED-Bri│LED-Sp│      │  Trans  │
  * ├──────┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬──────┤
  * │ MO(FN2) │      │  ▲   │      │      │      │      │      │      │      │ PSCR │ Home │  End │      │
- * ├─────────┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──────┤
+ * ├─────────┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴────────────┤
  * │   Trans    │  ◄   │  ▼   │  ►   │      │      │      │      │      │      │ PgUp │ PgDn │  Trans  │
- * ├────────────┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──────────┤
- * │    Trans      │      │      │      │      │      │      │      │      │ Ins  │ Del  │  Trans   │
- * ├──────┬────────┴──┬───┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──┬───┴──────┬─────┬────┤
+ * ├────────────┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴────────────────┤
+ * │    Trans      │      │      │      │      │      │      │      │      │ Ins  │ Del  │  Trans      │
+ * ├──────┬────────┴──┬───┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──┬───┴──────┬────────┬────┤
  * │ Trans│   Trans   │   Trans  │                 Trans                   │  Trans   │ FN1 │ FN2 │    │
  * └──────┴───────────┴──────────┴─────────────────────────────────────────┴──────────┴─────┴─────┴────┘
  */
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE_LAYER] = KEYMAP(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_UP),
-        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                    MO(_FN1_LAYER),   LT(_FN2_LAYER, KC_LEFT), KC_DOWN, KC_RGHT
-    ),
+    [_BASE_LAYER] = KEYMAP(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP), KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(_FN1_LAYER), LT(_FN2_LAYER, KC_LEFT), KC_DOWN, KC_RGHT),
 
-    [_FN1_LAYER] = KEYMAP(
-        KC_MEDIA_EJECT, KC_F1,   KC_F2,                  KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,              KC_F12,             KC_DEL,
-        KC_TRNS,        KC_DOT,  KC_RIGHT_ANGLE_BRACKET, KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_HOME,             KC_END,             KC_TRNS,
-        KC_CAPSLOCK,    KC_HOME, KC_TRNS,                KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN,             KC_TRNS,
-        KC_TRNS,        KC_TRNS, KC_TRNS,                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_BRIGHTNESS_DOWN,         KC_BRIGHTNESS_UP,
-        KC_TRNS,        KC_TRNS, KC_TRNS,                         KC_MEDIA_PLAY_PAUSE,                KC_TRNS,          KC_MEDIA_NEXT_TRACK,         KC_MEDIA_PREV_TRACK, KC_TRNS
-    ),
+    [_FN1_LAYER] = KEYMAP(KC_MEDIA_EJECT, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_TRNS, KC_DOT, KC_RIGHT_ANGLE_BRACKET, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_HOME, KC_END, KC_TRNS, KC_CAPSLOCK, KC_HOME, KC_TRNS, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_MEDIA_PLAY_PAUSE, KC_TRNS, KC_MEDIA_NEXT_TRACK, KC_MEDIA_PREV_TRACK, KC_TRNS),
 
-    [_FN2_LAYER] = KEYMAP(
-        KC_MEDIA_EJECT, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, KC_TRNS, KC_TRNS, KC_TRNS, KC_AP_LED_OFF, KC_AP_LED_ON, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED, KC_TRNS, KC_TRNS,
-        MO(_FN2_LAYER), KC_TRNS,    KC_UP,      KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,      KC_PSCR,                 KC_HOME,      KC_END,  KC_TRNS,
-        KC_TRNS,        KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,      KC_PGUP,                 KC_PGDN,      KC_TRNS,
-        KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_INS,       KC_DEL,                  KC_TRNS,
-        KC_TRNS,        KC_TRNS,    KC_TRNS,                             KC_TRNS,                             KC_TRNS,            MO(_FN1_LAYER),          MO(_FN2_LAYER), KC_TRNS
-    ),
+    [_FN2_LAYER] = KEYMAP(KC_MEDIA_EJECT, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, KC_TRNS, KC_TRNS, KC_TRNS, KC_AP_LED_OFF, KC_AP_LED_ON, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED, KC_TRNS, KC_TRNS, MO(_FN2_LAYER), KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_HOME, KC_END, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_FN1_LAYER), MO(_FN2_LAYER), KC_TRNS),
 };
 
 const uint16_t keymaps_size = sizeof(keymaps);
@@ -89,7 +71,6 @@ void matrix_scan_user(void) {}
 /* Restore active profile when waking up from USB sleep */
 void suspend_wakeup_init_user(void) {
     annepro2LedEnable();
-    annepro2LedSetProfile(0);
 }
 
 /* Keyboard post-initialization */
